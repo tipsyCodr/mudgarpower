acf92b10ff8ecc95e5f9054c25b0792882884177
Hemant Kumar <105009836+tipsyCodr@users.noreply.github.com>
2024-08-07 12:04:13 +0530

    added db credentials

--- root/db_connection.php
+++ root/db_connection.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+$user = ini_get('user');
+$pass = ini_get('pass');
+$db = ini_get('dbname');
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=mudgarpower;charset=utf8mb4', 'root', ''); 
+$db = new PDO('mysql:host=127.0.0.1;dbname=' . $dbname . ';charset=utf8mb4', $user, $pass);
 ?>
